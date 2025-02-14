d4a7ca0218302cd88f69684206e28fe696eee0fd
Mubarek <156066237+MubarekAdem@users.noreply.github.com>
2025-02-14 09:59:59 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@
 import { motion } from "framer-motion";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
 
 export default function Home() {
 
